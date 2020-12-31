6039c89cc95576d301162e4d384ab278d2cc09b3
daniel-cr <eldanielcr@gmail.com>
2020-12-30 20:03:27 -0600

    Update ITileDefinitionManager.cs

--- Robust.Shared/Interfaces/Map/ITileDefinitionManager.cs
+++ Robust.Shared/Interfaces/Map/ITileDefinitionManager.cs
@@ -32,13 +32,31 @@ namespace Robust.Shared.Interfaces.Map
         /// <summary>
         ///     Register a definition with this manager.
         /// </summary>
-        /// <param name="tileDef">THe definition to register.</param>
+        /// <param name="tileDef">The definition to register.</param>
         void Register(ITileDefinition tileDef);
 
+        /// <summary>
+        ///     Add base turfs to a lookup list
+        /// </summary>
+        /// <param name="baseTurf">The base turf to add to the list</param>
         void SetLookUp(string baseTurf);
 
+        /// <summary>
+        ///     Gets the tileId of the n base turf of the current tile, where n is the 'steps'
+        ///     from the current tile
+        /// </summary>
+        /// <param name="tileId">The current tileId of the tile definition</param>
+        /// <param name="steps">The level of base turf</param>
+        /// <returns>The TileId of the n base turf</returns>
         ushort GetBaseTurfId(ushort tileId, int steps=1);
 
+        /// <summary>
+        ///     Gets the tile definition of the n base turf of the current tile, where n is the 'steps'
+        ///     from the current tile
+        /// </summary>
+        /// <param name="tileId">The current tileId of the tile definition</param>
+        /// <param name="steps">The level of base turf</param>
+        /// <returns>The Tile definition of the n base turf</returns>
         ITileDefinition GetBaseTurfTileDef(ushort tileId, int steps=1);
 
     }
